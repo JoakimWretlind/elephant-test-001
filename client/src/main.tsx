dafbf196dcb6fad17bdc074b68ab39b8ce6d364d
JoakimWretlind <joakim.wretlind@gmail.com>
2023-04-25 16:06:35 +0200

    added dependencies if production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
